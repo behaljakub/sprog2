98e40b405faec745da31e51e0e2721b6d7490236
behaljakub <behalj@gjs.cz>
2024-11-30 15:07:45 +0100

    Update povrchvalce.py
    změna proměnných a úprava

--- promenne/povrchvalce.py
+++ promenne/povrchvalce.py
@@ -1,6 +1,6 @@
 import math
-vyska = int(input("Výška Válce: "))
-polomer = int(input("Polomer Válce: "))
-vysledek = 2*math.pi*vyska*(polomer + vyska)
-zaokrouhleno = round(vysledek,2)
-print(f"Povrch Válce:", zaokrouhleno)
+height = float(input("Výška Válce: "))
+radius = float(input("Polomer Válce: "))
+vysledek = 2*math.pi*height*(radius + height)
+roundedoff = round(vysledek,2)
+print(f"Povrch Válce:, {roundedoff} cm²")
