4f6a53c05e9657e0b5ab7b0829f9c3a865fe698f
behaljakub <behalj@gjs.cz>
2024-11-30 20:01:13 +0100

    s

---
+++ promenne/prumerna_rychlost.py
@@ -0,0 +1,10 @@
+import math 
+
+vzdalenost_km = float(input("Zadejte ujetou vzdálenost v kilometrech: "))
+hodiny = int(input("Zadejte počet hodin: "))
+minuty = int(input("Zadejte počet minut: "))
+celkovy_cas = hodiny + minuty/60
+
+prumer_rych = vzdalenost_km/celkovy_cas
+
+print(f"Průměrná rychlost je: {prumer_rych} km/h")
