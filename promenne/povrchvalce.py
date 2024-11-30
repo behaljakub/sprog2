5a58105740d80370c3bb9102b08661c67aef3afb
behaljakub <behalj@gjs.cz>
2024-11-30 15:53:35 +0100

    s

--- promenne/povrchvalce.py
+++ promenne/povrchvalce.py
@@ -1,6 +1,5 @@
 import math
 height = float(input("Výška Válce: "))
 radius = float(input("Polomer Válce: "))
-vysledek = 2*math.pi*height*(radius + height)
-roundedoff = round(vysledek,2)
-print(f"Povrch Válce:, {roundedoff} cm²")
+result = 2*math.pi*height*(radius + height)
+print(f"Povrch Válce:, {result} cm²")
--- promenne/stahovanidat.py
+++ promenne/stahovanidat.py
@@ -1,1 +1,7 @@
-objem = int(input("Objem dat: " ))
+import math
+volume = float(input("Objem dat v MB: "))
+speed = float(input("Rychlost stahovaní v Mbit/s: "))
+time = volume*8/speed
+roundoff = round(time, 2)
+print(f"Doba stáhování je: {roundoff}s")
+
