f0b92af4e88398762451bc463e496c4817a9d575
behaljakub <behalj@gjs.cz>
2024-12-17 19:27:06 +0100

    Create serazeni.py

---
+++ podminky/serazeni.py
@@ -0,0 +1,11 @@
+cislo1 = float(input("Zadejte první čislo: "))
+cislo2 = float(input("Zadejte druhé čislo: "))
+cislo3 = float(input("Zadejte třetí číslo: "))
+
+if cislo1 >= cislo2 >= cislo3:
+    if cislo1 >= cislo3:
+        print(f"Čísla v zestupném pořadí {cislo1, cislo2, cislo3}")
+
+if cislo2 >= cislo1 >= cislo3
+    if cislo2 >= cislo3
+        print(f"Čísla v sestupném pořadí {cislo2, c}")
