fe7fdf5926ba2e6c0b64b0f8dafa34a94c3348b8
behaljakub <behalj@gjs.cz>
2024-12-17 19:39:10 +0100

    ss

---
+++ podminky/kvadrrovnice.py
@@ -0,0 +1,7 @@
+import math
+
+a = float(input("Koeficient a: "))
+b = float(input("Koeficient b: "))
+c = float(input("Koeficient c: "))
+
+if 
--- podminky/serazeni.py
+++ podminky/serazeni.py
@@ -2,10 +2,23 @@ cislo1 = float(input("Zadejte první čislo: "))
 cislo2 = float(input("Zadejte druhé čislo: "))
 cislo3 = float(input("Zadejte třetí číslo: "))
 
-if cislo1 >= cislo2 >= cislo3:
-    if cislo1 >= cislo3:
-        print(f"Čísla v zestupném pořadí {cislo1, cislo2, cislo3}")
+if cislo1 >= cislo2 and cislo1 >= cislo3:
+    if cislo2 >= cislo3:
+        sestupne = (cislo1, cislo2, cislo3)
+    else:
+        sestupne = (cislo1, cislo3, cislo2)
 
-if cislo2 >= cislo1 >= cislo3
-    if cislo2 >= cislo3
-        print(f"Čísla v sestupném pořadí {cislo2, c}")
+elif cislo2 >= cislo1 and cislo2 >= cislo3:
+    if cislo1 >= cislo3:
+        sestupne = (cislo2, cislo1, cislo3)
+    else:
+        sestupne = (cislo2, cislo3, cislo1)
+
+else:
+    if cislo1 >= cislo2:
+        sestupne = (cislo3, cislo1, cislo2)
+    else:
+        sestupne = (cislo3, cislo2, cislo1)
+
+print(f"Čísla seřazena sestupně:{sestupne}")
+    
