5764d32479d71d08e017a8d36a001a4c6cd8b6ef
behaljakub <behalj@gjs.cz>
2025-01-20 18:41:38 +0100

    t

---
+++ podminky/kvadranty.py
@@ -0,0 +1,25 @@
+import math
+
+x = float(input("Zadejte první číslo: "))
+y = float(input("Zadejte druhé číslo: "))
+
+if x >= 0 and y >= 0:
+    print("Bod A se nachází v I kvadrantu")
+
+elif x <= 0 and y >=0:
+    print("Bod A se nachází v II kvadrantu")
+
+elif x <= 0 and y <= 0:
+    print("Bod A se nachází v III kvadrantu")
+
+else:
+    print("Bod A se nachází v IV kvadrantu")
+
+
+
+
+
+
+
+
+
--- podminky/kvadrrovnice.py
+++ podminky/kvadrrovnice.py
@@ -4,4 +4,19 @@ a = float(input("Koeficient a: "))
 b = float(input("Koeficient b: "))
 c = float(input("Koeficient c: "))
 
-if 
+D = b**2 - 4*a*c
+x1 = (-b + D**0.5)/2*a
+x2 = (-b - D**0.5)/2*a
+
+if D > 0:
+    print(f"Kořeny rovnice jsou {x1}, {x2}")
+
+elif D == 0:
+    print(f"Kořeny rovnice jsou {x1}, {x2}")
+
+else:
+    D < 0
+    print("Kořeny rovnice nejsou definovány")
+
+
+
