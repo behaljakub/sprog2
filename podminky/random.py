73abde579dc1093c978ca60e9bf5de57d9b049d1
behaljakub <behalj@gjs.cz>
2025-01-22 21:36:25 +0100

    xd

---
+++ podminky/random.py
@@ -0,0 +1,7 @@
+n1 = float(input("Zadejte první číslo: "))
+n2 = float(input("Zadejte druhé číslo: "))
+n3 = float(input("Zadejte třetí číslo: "))
+
+if n1 >= n1 and n2 >= n3:
+    if n2 >= n3:
+        print(f"Čísla sestupně: {n1}, {n2}, {n3}")
