6292166fc63288e71a5d8d704eb4f042c33bb738
behaljakub <behalj@gjs.cz>
2025-02-13 21:04:05 +0100

    s

--- listy/random.py
+++ listy/random.py
@@ -1,21 +1,31 @@
-cislo = [2, 3, 1, 0, 5, 6, 4]
+cislo = [2, 3, 1, 0, 5, 6, 4, 9]
 
-maxn = 0
+maxn = cislo[0]
 minn = cislo[0]
-sum = 0
-count = 0
 
 for i in cislo:
     if i > maxn:
         maxn = i
-    
-    if i < minn:
-        minn = i
-    
-    count += 1
-    sum += 1
 
-print(count)
+print(maxn)
+
+for n in cislo:
+    if n < minn:
+        minn = n 
+
+print(minn)
+
+soucet = 0
+
+for k in cislo:
+    soucet += k
+
+
+
+print(soucet/len(cislo))
+
+
+    
 
 
 
