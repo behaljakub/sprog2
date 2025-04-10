bb68b7a973b95ba6e2ec0a951e6cf46dea7e16d8
behaljakub <behalj@gjs.cz>
2025-04-10 07:03:04 +0200

    Update nejdelsislovo.py

---
+++ funkce/nejdelsislovo.py
@@ -0,0 +1,8 @@
+def nejdelsi_slovo(text)
+    text.split()
+    nejdelsi = ""
+    for slovo in text:
+        if len(nejdelsi) < len(slovo)
+        nejdelsi = slovo
+        
+print(nejdelsi_slovo("Včera jsme šli na výlet do hor"))
