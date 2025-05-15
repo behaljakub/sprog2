0d900a9adc7c06a65477c080560d37fdc713bffb
behaljakub <behalj@gjs.cz>
2025-05-15 12:46:54 +0200

    s

---
+++ rekurze/fibonacci.py
@@ -0,0 +1,15 @@
+cisla = {}
+
+def fib(n):
+    if n in cisla:
+        return cisla[n]
+    if n == 0:
+        cisla[n] = 0
+    elif n == 1:
+        cisla[n] = 1
+    else:
+        cisla[n] = fib(n - 1) + fib(n - 2)
+    return cisla[n]
+
+print(fib(100))
+
---
+++ testy/e.py
@@ -0,0 +1,16 @@
+def konvertor_men(castka, z_meny, na_menu, kurzy=None):
+    vychozi_kurzy = {
+    "CZK": 1.0,
+    "EUR": 0.04,
+    "USD": 0.043,
+    "GBP": 0.034,
+    "JPY": 6.43
+    }
+    
+    if kurzy == None:
+        kurzy = vychozi_kurzy
+
+
+
+
+
