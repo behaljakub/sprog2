e52df93f6eecca5772fad4b5c98456f430c7ac01
behaljakub <behalj@gjs.cz>
2025-05-22 12:57:21 +0200

    s

--- testy/e.py
+++ testy/e.py
@@ -1,25 +1,30 @@
 def konvertor_men(castka, z_meny, na_menu, kurzy=None):
     vychozi_kurzy = {
-    "CZK": 1.0, #1 czk = 0.04 eur
-    "EUR": 0.04, #1 eur = 0.04 czk
-    "USD": 0.043, # 1dol = 0.043 czk
+    "CZK": 1.0,
+    "EUR": 0.04,
+    "USD": 0.043,
     "GBP": 0.034,
     "JPY": 6.43
     }
     
     if kurzy == None:
         kurzy = vychozi_kurzy
-    if z_meny not in kurzy or na_menu not in kurzy or castka < 0:
+    
+    if castka <= 0 or z_meny not in vychozi_kurzy or na_menu not in vychozi_kurzy:
         return None
     
-    castka_ve_stalem_kurzu = castka/kurzy[z_meny]
-    vysledna_castka = castka_ve_stalem_kurzu*kurzy[na_menu]
-    return round(vysledna_castka, 2)
+    prvni_prevod = castka/kurzy[z_meny]
+    druhy_prevod = prvni_prevod*kurzy[na_menu]
+
+
+    vysledek = round(druhy_prevod, 2)
+
+    return vysledek
 
 print(konvertor_men(1000, "CZK", "EUR"))  # 40.0
 print(konvertor_men(50, "EUR", "CZK"))  # 1250.0
 print(konvertor_men(100, "USD", "JPY"))  # 14953.49
-print(konvertor_men(75, "GBP", "USD", {"GBP": 1.26, "USD": 1.0}))  # 94.5 spatne 59.52
+print(konvertor_men(75, "GBP", "USD", {"GBP": 1.00, "USD": 1.26}))  # 94.5
 print(konvertor_men(200, "CZK", "XYZ"))  # None
 
 
---
+++ testy/f.py
@@ -0,0 +1,32 @@
+import random
+
+
+def uziv_slovo(slovo):
+    znaky = []
+    for znak in slovo:
+        znaky += [znak]
+
+
+
+    for x in range(1, 100):
+        poz1 = random.randint(0, len(slovo) - 1)
+        poz2 = random.randint(0, len(slovo) - 1)
+
+        a = znaky[poz1]
+        b = znaky[poz2]
+
+        znaky[poz1] = b
+        znaky[poz2] = a
+
+    nove_slovo = ""
+    for y in znaky:
+        nove_slovo += y
+    
+    return nove_slovo
+
+slovo = input("Zadej slovo:")
+
+
+print(uziv_slovo(slovo))
+
+
