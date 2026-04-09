c13e71fed1511d6dd0492495d081b147f91be2ad
behaljakub <behalj@gjs.cz>
2026-04-09 12:57:14 +0200

    s

--- maturita/4.py
+++ maturita/4.py
@@ -1,1 +1,12 @@
-def rle(seznam):
+def eratosthenovo_sito(n):
+    seznam = []
+    for x in range(2, n + 1):
+        seznam.append(x)
+
+    
+    for i in seznam:
+        for k in range(i*2, n+1, i):
+            if k in seznam:
+                seznam.remove(k)
+    return seznam
+print(eratosthenovo_sito(10))
