0e76eeb788579a996fe68c45ba00bf2b518d1127
behaljakub <behalj@gjs.cz>
2026-04-16 13:04:53 +0200

    s

--- project.py
+++ project.py
@@ -1,27 +1,18 @@
-import math
+import random
 
-class Zlomek:
-    def __init__(self, citatel, jmenovatel=1):
-        self.citatel = citatel
-        self.jmenovatel = jmenovatel
+delka = int(input("Zadej délku seznamu: "))
 
-    def __str__(self):
-        return (f"{self.citatel}/{self.jmenovatel}")
-    
-    def zkrat(self):
-        nsd = math.gcd(abs(self.citatel), abs(self.jmenovatel))
-        print(nsd)
-        self.citatel//nsd
-        self.jmenovatel//nsd
-        return self
-    
-z4 = Zlomek(8, 12)
-print(f"Před zkrácením: {z4}")  # 8/12
-z4.zkrat()
-print(f"Po zkrácení: {z4}") 
-    
+cisla = []
+for i in range(delka):
+    cislo = random.randint(1, 20)
+    cisla.append(cislo)
 
-    
+print(cisla)
 
+novy_seznam = []
 
-
+for x in cisla:
+    if cisla.count(x) == 1:
+        novy_seznam.append(x)
+  
+print(novy_seznam)
