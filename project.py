44683381f718ee26da9c7a620dfa0a25e4645f03
behaljakub <behalj@gjs.cz>
2026-04-30 12:57:04 +0200

    s

--- project.py
+++ project.py
@@ -1,18 +1,63 @@
-import random
+import math
 
-delka = int(input("Zadej délku seznamu: "))
+class Zlomek:
+    def __init__(self, citatel, jmenovatel = 1):
+        self.citatel = citatel
+        self.jmenovatel = jmenovatel
 
-cisla = []
-for i in range(delka):
-    cislo = random.randint(1, 20)
-    cisla.append(cislo)
-
-print(cisla)
-
-novy_seznam = []
-
-for x in cisla:
-    if cisla.count(x) == 1:
-        novy_seznam.append(x)
-  
-print(novy_seznam)
+    def __str__(self):
+        return f"{self.citatel}/{self.jmenovatel}"
+    
+    def zkrat(self):
+        nsd = math.gcd(abs(self.citatel), abs(self.jmenovatel))
+        self.citatel //= nsd
+        self.jmenovatel //= nsd
+        return self
+    
+    def __add__(self, jiny):
+        novy_citatel = (self.citatel*jiny.jmenovatel) + (self.jmenovatel*jiny.citatel)
+        novy_jmenovatel = self.jmenovatel*jiny.jmenovatel
+        vysledek = Zlomek(novy_citatel, novy_jmenovatel)
+        return vysledek.zkrat()
+    
+    def __sub__(self, jiny):
+        novy_citatel = (self.citatel*jiny.jmenovatel) - (self.jmenovatel*jiny.citatel)
+        novy_jmenovatel = self.jmenovatel*jiny.jmenovatel
+        vysledek = Zlomek(novy_citatel, novy_jmenovatel)
+        return vysledek.zkrat()
+    
+    def __mul__(self, jiny):
+        novy_citatel = self.citatel*jiny.citatel
+        novy_jmenovatel = self.jmenovatel*jiny.jmenovatel
+        vysledek = Zlomek(novy_citatel, novy_jmenovatel)
+        return vysledek.zkrat()
+    
+    def __truediv__(self, jiny):
+        if jiny.jmenovatel != 0:
+            novy_citatel = self.citatel*jiny.jmenovatel
+            novy_jmenovatel = self.jmenovatel*jiny.citatel
+            vysledek = Zlomek(novy_citatel, novy_jmenovatel)
+        return vysledek.zkrat()
+    
+# Test 1: Vytvoření zlomků
+z1 = Zlomek(1, 2)
+z2 = Zlomek(3, 4)
+z3 = Zlomek(5)
+print(f"z1 = {z1}")  # 1/2
+print(f"z2 = {z2}")  # 3/4
+print(f"z3 = {z3}")  # 5/1
+# Test 2: Sčítání
+print(f"{z1} + {z2} = {z1 + z2}")  # 5/4
+# Test 3: Odčítání
+print(f"{z2} - {z1} = {z2 - z1}")  # 1/4
+# Test 4: Násobení
+print(f"{z1} * {z2} = {z1 * z2}")  # 3/8
+# Test 5: Dělení
+print(f"{z1} / {z2} = {z1 / z2}")  # 2/3
+# Test 6: Složitější výpočet
+print(f"({z1} + {z2}) * {z3} = {(z1 + z2) * z3}")  # 25/4
+# Test 7: Zkracování
+z4 = Zlomek(8, 12)
+print(f"Před zkrácením: {z4}")  # 8/12
+z4.zkrat()
+print(f"Po zkrácení: {z4}")    # 2/3
---
+++ zasobnik_opakovani.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        seznam = self.seznam = []
+    
+    def push(self, prvek):
+        self.seznam.append(prvek)
+        return self.seznam
+    
+    def pop(self):
+        # Odebere a vrátí prvek z vrcholu
+        # Nezapomeň zkontrolovat, jestli není prázdný!
+        pass
+    
+    def peek(self):
+        # Podívá se na vrchol, ale neodebere
+        pass
+    
+    def isEmpty(self):
+        # Vrátí True pokud je prázdný, jinak False
+        pass
